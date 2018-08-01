fd30eb47898ec527b69ec2df85438b8466518655
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-07-31 17:13:37 -0700

    first integration test

--- server.js
+++ server.js
@@ -9,4 +9,40 @@ app.use(express.static('public'));
 
 // app is listening on local port 8080
 // process.env.PORT is required for heroku env
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+
+//declare the server
+let server;
+// start the server and return a promise
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app
+        .listen(port, ()=> {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        })
+        .on("error", err => {
+            reject(err);
+        })
+    })
+}
+// close the server and return a promise
+function closeServer() {
+    return new Promise ((resolve, reject) => {
+        console.log("server shutdown");
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        })
+    })
+}
+
+if(require.main === module){
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,40 @@
+// declare and use chai, a js assertion library
+const chai = require("chai");
+// declare and use chai-http, a plugin for chai
+// for integration testing external apis
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+// declare and use should interface
+const should = chai.should();
+// declare and import app, runServer, and closeServer
+const {app, runServer, closeServer} = require("../server");
+
+
+describe("Server is running", function() {
+    
+    //run server before running tests
+    before(function() {
+        return runServer();
+    })
+    //close server after running tests
+    after(function() {
+        return closeServer();
+    })
+
+    it("should return a response", function() {
+        return chai
+        .request(app)
+        .get("/")
+        .then(function(res){
+            res.should.have.status(200);
+        })
+    })
+    it("should display html file", function() {
+        return chai
+        .request(app)
+        .get("/")
+        .then(function(res) {
+            res.should.be.html;
+        })
+    })
+})
