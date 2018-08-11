1a9dcdfab60012739348b8cbaa5d5f20cd4f0155
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-08-10 17:35:17 -0700

    updated test url

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
     'mongodb://admin:admin123@ds247619.mlab.com:47619/birds-nest';
 exports.TEST_DATABASE_URL = 
     process.env.TEST_DATABASE_URL ||
-    'mongodb://localhost/test-birds-nest-db';
+    'mongodb://admintest:test123@ds119442.mlab.com:19442/birds-nest-test';
 exports.PORT = process.env.PORT || 8080;
